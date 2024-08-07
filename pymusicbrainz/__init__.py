91aeb54d35dc0d15584d3f9e62474ec48f2bf97d
Peter van Liesdonk <peter@liesdonk.nl>
2024-08-07 14:46:36 +0200

    Extra imports

--- pymusicbrainz/__init__.py
+++ pymusicbrainz/__init__.py
@@ -2,7 +2,8 @@ from .datatypes import (ArtistID, ReleaseGroupID, ReleaseID, RecordingID, WorkID
 
 from .constants import UNKNOWN_ARTIST_ID, VA_ARTIST_ID
 
-from .object_cache import (get_artist, get_medium, get_track, get_recording, get_work, get_release, get_release_group, clear_object_cache)
+from .object_cache import (get_artist, get_medium, get_track, get_recording, get_work, get_release, get_release_group,
+                           clear_object_cache)
 
 from .config import configure_database, configure_musicbrainzngs, configure_typesense, is_configured_musicbrainzngs
 
@@ -12,4 +13,9 @@ from .search import (
     search_song, search_song_musicbrainz, search_song_canonical, search_artist_musicbrainz,
     search_by_recording_id, search_fingerprint
 
+)
+
+from .dataclasses import (
+    Artist, Medium, Track, Recording, Work, Release, ReleaseGroup, MusicbrainzSingleResult, MusicbrainzListResult,
+    MusicbrainzSearchResult
 )
